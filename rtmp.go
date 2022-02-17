dbefdc2a9a4b500ce2aacf6058dcf542144efc28
joepadmiraal <joep@groovytunes.nl>
2022-02-17 17:43:49 +0100

    Removed a the ack log line

--- rtmp.go
+++ rtmp.go
@@ -1384,7 +1384,7 @@ func (self *Conn) readChunk() (err error) {
 	}
 
 	if shouldAck {
-		self.logToExtern(fmt.Sprintf("Writing ACK because: %s, bytes: %d, self.readAckSize: %d, conn: %s", ackReason, self.ackn, self.readAckSize, self.netconn.RemoteAddr().String()))
+		// self.logToExtern(fmt.Sprintf("Writing ACK because: %s, bytes: %d, self.readAckSize: %d, conn: %s", ackReason, self.ackn, self.readAckSize, self.netconn.RemoteAddr().String()))
 		if err = self.writeAck(self.ackn); err != nil {
 			return
 		}
